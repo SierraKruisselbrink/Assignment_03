0f30ddaa6eb29f3ac415e28967d27162c3bc3790
Sierra Kruisselbrink <sierra.kruisselbrink@uleth.ca>
2024-11-29 13:10:22 -0700

    added standard colors and a color picker

--- js/scripts.js
+++ js/scripts.js
@@ -45,7 +45,7 @@ const ctx = canvas.getContext('2d');
 // Set canvas size
 function resizeCanvas() {
     canvas.width = window.innerWidth * .80;
-    canvas.height = window.innerHeight * .80;
+    canvas.height = window.innerHeight *.75;
 }
 
 resizeCanvas();
@@ -167,4 +167,47 @@ $("#eraser-brush").click(function () {
 eraserBrushButton.addEventListener('click', () => {
     handleButtonClick(eraserBrushButton);
     currentBrush = new Brush('eraser', currentBrush.size, '#FFFFFF'); // Assuming white background
+});
+
+
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'classic', // or 'monolith', or 'nano'
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: true,
+            hsla: true,
+            hsva: true,
+            cmyk: true,
+            input: true,
+            clear: true,
+            save: true
+        }
+    }
 });

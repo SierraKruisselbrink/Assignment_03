aafb5c738555298c7f876d5d8e50dde80dfe878f
Sierra Kruisselbrink <sierra.kruisselbrink@uleth.ca>
2024-12-04 13:36:58 -0700

    Added theme button highlights

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,9 @@
-// Create a class for Brush
-var backgroundLightMode = '#FFFFFF';
-var backgroundDarkMode = '#171717';
-
-const christmasColors = [
-    'rgb(48, 42, 10)', // Brown
-    'rgb(98, 22, 34)', // Dark Red
-    'rgb(110, 50, 13)', // Brown
-    'rgb(159, 15, 14)', // Cherry Red 
-    'rgb(201, 40, 30)', // Bright Red 
-    'rgb(154, 49, 72)', // Red
-    'rgb(193, 108, 129)', // Pink
-    'rgb(187, 109, 33)', // Dark Yellow
-    'rgb(179, 111, 64)', // Light Brown
-    'rgb(114, 117, 97)', // Grey
-    'rgb(180, 182, 169)', // Light Grey
-    'rgb(214, 183, 152)', // Beige
- 
-    'rgb(25, 45, 36)', // Dark Green
-    'rgb(43, 72, 54)', // Green
-    'rgb(55, 88, 57)', // Olive Green 
-    'rgb(31, 81, 32)', // Bright Green
-    'rgb(123, 144, 49)', // Yellow Green
-    'rgb(131, 160, 67)', // Light Green
-    'rgb(68, 126, 150)', // Blue
-    'rgb(107, 146, 163)', // Light Blue
-    'rgb(112, 142, 118)', // Light Blue
-    'rgb(195, 200, 204)', // Light Grey snow
-    'rgb(227, 227, 227)', // Snow
-    'rgb(251, 205, 58)' // Yellow
- 
-    ];
-
-// Store original colors of color circles
-const originalColors = [];
-document.querySelectorAll('.color-circle').forEach(circle => {
-    originalColors.push(circle.style.backgroundColor);
-});
 
 
 
 class Brush {
     constructor(brushType, size, newColor) {
-        this.brushType = brushType; // 'line' or 'circle'
+        this.brushType = brushType; 
         this.size = size;
         this.color = newColor;
     }
@@ -80,14 +42,6 @@ class Brush {
 const canvas = document.getElementById('drawing-canvas');
 const ctx = canvas.getContext('2d');
 
-
-
-// Set canvas size
-function resizeCanvas() {
-    canvas.width = window.innerWidth * .80;
-    canvas.height = window.innerHeight * .75;
-}
-
 resizeCanvas();
 
 window.addEventListener('resize', resizeCanvas);
@@ -110,14 +64,8 @@ const eraserBrushButton = document.getElementById('eraser-brush');
 const lightThemeButton = document.getElementById('light-theme');
 const darkThemeButton = document.getElementById('dark-theme');
 
-// Toggle light mode
-lightThemeButton.addEventListener('click', () => {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.remove('christmas-theme');
-    document.body.classList.add('light-mode');
-    restoreOriginalColors();
 
-});
+
 
 function restoreOriginalColors() {
     const colorCircles = document.querySelectorAll('.color-circle');
@@ -127,14 +75,54 @@ function restoreOriginalColors() {
     });
 }
 
+function setTheme(pageColor, canvasColor, buttonColor) {
+    document.body.style.backgroundColor = pageColor;
+    setCanvasBackgroundColor(canvasColor);
+    setButtonBackgroundColor(buttonColor);
+    currentBackgroundColor = backgroundColor;
+}
+
+function handleThemeButtonClick(button) {
+    // Remove 'active' class from all theme buttons
+    document.querySelectorAll('.theme-button').forEach(btn => btn.classList.remove('active'));
+    // Add 'active' class to the clicked button
+    button.classList.add('active');
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth * .80;
+    canvas.height = window.innerHeight * .75;
+}
+
+function handleButtonClick(button) {
+    $('.brush-button').removeClass('active');
+    $('.brush-button').removeClass('active-eraser');
+    if (button.id === 'eraser-brush') {
+        $(button).addClass('active-eraser');
+    } else {
+        $(button).addClass('active');
+    }
+}
+
+// Toggle light mode
+lightThemeButton.addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.remove('christmas-theme');
+    document.body.classList.add('light-mode');
+    restoreOriginalColors();
+    document.querySelector('.color-palette-container').classList.remove('christmas-palette');
+    handleThemeButtonClick(lightThemeButton);
+});
+
 // Toggle dark mode
 darkThemeButton.addEventListener('click', () => {
     document.body.classList.remove('light-mode');
     document.body.classList.remove('christmas-theme');
     document.body.classList.add('dark-mode');
     restoreOriginalColors();
+    document.querySelector('.color-palette-container').classList.remove('christmas-palette');
+    handleThemeButtonClick(darkThemeButton);
 });
-
 
 // Update brush size
 decreaseSizeButton.addEventListener('click', () => {
@@ -150,6 +138,7 @@ increaseSizeButton.addEventListener('click', () => {
         brushSizeValue.textContent = currentBrush.size;
     }
 });
+
 
 // Update brush color
 colorCircles.forEach(circle => {
@@ -168,26 +157,6 @@ circleBrushButton.addEventListener('click', () => {
     handleButtonClick(this);
     currentBrush = new Brush('circle', currentBrush.size, currentBrush.color);
 });
-
-// eraserBrushButton.addEventListener('click', () => {
-//     handleButtonClick(eraserBrushButton);
-//     currentBrush = new Brush('eraser', currentBrush.size, '#FFFFFF'); // Assuming white background
-// });
-
-
-function handleButtonClick(button) {
-    // Remove 'active' class from all buttons
-    $('.brush-button').removeClass('active');
-    $('.brush-button').removeClass('active-eraser');
-    // Add 'active' class to the clicked button
-
-    if (button.id === 'eraser-brush') {
-        $(button).addClass('active-eraser');
-    } else {
-        $(button).addClass('active');
-    }
-}
-
 
 
 
@@ -221,61 +190,45 @@ clearCanvasButton.addEventListener("click", () => {
 })
 
 
-
-
-
-
 //Eraser Button
 eraserBrushButton.addEventListener('click', () => {
     handleButtonClick(eraserBrushButton);
     currentBrush = new Brush('eraser', currentBrush.size, '#FFFFFF'); // Assuming white background
 });
 
-
+// Color picker, taken from https://simonwep.github.io/pickr/
 const pickr = Pickr.create({
     el: '.color-picker-button',
     theme: 'classic', // or 'monolith', or 'nano'
     default: '#000000',
     useAsButton: true,
 
-
-    swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)'
-    ],
-
     components: {
 
         // Main components
         preview: true,
-        opacity: true,
+        opacity: false,
         hue: true,
 
         // Input / output Options
         interaction: {
             hex: true,
             rgba: true,
-            hsla: true,
-            hsva: true,
-            cmyk: true,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
             input: true,
             clear: true,
             save: true
         }
     }
 });
+
+pickr.on('save', (color, instance) => {
+    currentBrush.setBrushColor(color.toHEXA().toString());
+    pickr.hide();
+});
+
 
 // Add event listeners to brush buttons
 $('.brush-button').click(function () {
@@ -285,25 +238,10 @@ $('.brush-button').click(function () {
 // Add event listener to eraser button
 $('#eraser-brush').click(function () {
     handleButtonClick(this);
-    // currentBrush = new Brush('eraser', currentBrush.size, '#FFFFFF'); // Assuming white background
-});
+    });
 
-// Clear Canvas button
-$('#clear-canvas').click(function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
 
-pickr.on('save', (color, instance) => {
-    currentBrush.setBrushColor(color.toHEXA().toString());
-    pickr.hide();
-});
 
-function setTheme(pageColor, canvasColor, buttonColor) {
-    document.body.style.backgroundColor = pageColor;
-    setCanvasBackgroundColor(canvasColor);
-    setButtonBackgroundColor(buttonColor);
-    currentBackgroundColor = backgroundColor;
-}
 
 // Christmas theme
 document.getElementById('christmas-theme').addEventListener('click', () => {
@@ -315,6 +253,8 @@ document.getElementById('christmas-theme').addEventListener('click', () => {
         circle.style.backgroundColor = christmasColors[index % christmasColors.length];
         circle.dataset.color = christmasColors[index % christmasColors.length];
     });
+    document.querySelector('.color-palette-container').classList.add('christmas-palette');
+    handleThemeButtonClick(document.getElementById('christmas-theme'));
 });
 
 function setCanvasBackgroundColor(color) {
@@ -330,3 +270,39 @@ function setButtonBackgroundColor(color) {
         button.style.backgroundColor = color;
     });
 }
+
+
+const christmasColors = [
+    'rgb(48, 42, 10)', // Brown
+    'rgb(98, 22, 34)', // Dark Red
+    'rgb(110, 50, 13)', // Brown
+    'rgb(159, 15, 14)', // Cherry Red 
+    'rgb(201, 40, 30)', // Bright Red 
+    'rgb(154, 49, 72)', // Red
+    'rgb(193, 108, 129)', // Pink
+    'rgb(187, 109, 33)', // Dark Yellow
+    'rgb(179, 111, 64)', // Light Brown
+    'rgb(114, 117, 97)', // Grey
+    'rgb(180, 182, 169)', // Light Grey
+    'rgb(214, 183, 152)', // Beige
+ 
+    'rgb(25, 45, 36)', // Dark Green
+    'rgb(43, 72, 54)', // Green
+    'rgb(55, 88, 57)', // Olive Green 
+    'rgb(31, 81, 32)', // Bright Green
+    'rgb(123, 144, 49)', // Yellow Green
+    'rgb(131, 160, 67)', // Light Green
+    'rgb(68, 126, 150)', // Blue
+    'rgb(107, 146, 163)', // Light Blue
+    'rgb(112, 142, 118)', // Light Blue
+    'rgb(195, 200, 204)', // Light Grey snow
+    'rgb(227, 227, 227)', // Snow
+    'rgb(251, 205, 58)' // Yellow
+ 
+    ];
+
+// Store original colors of color circles
+const originalColors = [];
+document.querySelectorAll('.color-circle').forEach(circle => {
+    originalColors.push(circle.style.backgroundColor);
+});
